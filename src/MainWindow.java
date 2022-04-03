711504157fb04cbbeb294669a11258c4f40d50c2
IndiRow <39675003+IndiRow@users.noreply.github.com>
2022-04-03 22:47:20 +0600

    Update MainWindow.java

--- src/MainWindow.java
+++ src/MainWindow.java
@@ -1,17 +1,17 @@
-import javax.swing.*;
+import javax.swing.*
 
 public class MainWindow extends JFrame {
 
     public MainWindow(){
-        setTitle("Snake");
-        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        setSize(400,400);
-        setLocation(400,400);
-        add(new GameWindow());
-        setVisible(true);
+        setTitle("Snake")
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE)
+        setSize(400, 400)
+        setLocation(400, 400)
+        add(new GameWindow())
+        setVisible(true)
     }
 
     public static void main(String[] args) {
-        MainWindow mw = new MainWindow();
+        MainWindow mw = new MainWindow()
     }
 }
